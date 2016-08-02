063bdb7816bca1895366164e54f5efb717f84f17
Rachel Gardner <rachelgardner9@gmail.com>
2016-08-01 22:16:19 -0500

    Removed old files (under different naming system). We promise to now use Git for its intended purpose)

--- database/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_HOST', 'localhost');
-define('DB_DATABASE', 'croma');
-define('DB_USERNAME', 'croma');
-define('DB_PASSWORD', 'tiger6710');
-
-?>
--- database/db_functionsTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once("croma_dbFunctions.php");
-include_once("drupalCompatibility.php");
-
-print_r(dbCreateTeam(array("name" => "test"));
-
-?>
